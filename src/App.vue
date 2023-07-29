245f55027e9168920234bab9e02bc6830501e306
Zenen Peraza <peraza@outlook.com>
2023-07-28 21:18:00 -0400

    estructura y layauts en Ionic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <ion-menu content-id="main-content">
+
+  <ion-page >
     <ion-header>
       <ion-toolbar>
-        <ion-title>Menu Content</ion-title>
+        <ion-title>GRID Example</ion-title>
       </ion-toolbar>
+       
     </ion-header>
-    <ion-content class="ion-padding">This is the menu content.</ion-content>
-  </ion-menu>
-  <ion-page id="main-content">
-    <ion-header>
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button></ion-menu-button>
-        </ion-buttons>
-        <ion-title>Menu</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding"> Tap the button in the toolbar to open the menu. </ion-content>
+    <ion-content class="ion-padding"> 
+      
+      <ion-grid>
+        <ion-row>
+          <ion-col size="12" class="full">Hello world full with</ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col size="6" size-xs="12" size-md="12" size-lg="6"  class="mid">50%</ion-col>
+          <ion-col size="6"  size-xs="12" size-md="12" size-lg="6" class="mid2">50%</ion-col>
+        </ion-row>
+      </ion-grid>
+
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-  import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import { IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/vue';
 
 
 
 </script>
+<style scoped>
+.full{
+  background-color: aqua;
+}
+.mid {
+  background-color: blueviolet;
+}
+.mid2 {
+  background-color: red;
+}
+</style>
